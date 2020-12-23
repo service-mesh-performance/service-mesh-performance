beedbd4aebf0e07db079e7c2e2bbb47c8c05e576
dhruv0000 <patel.4@iitj.ac.in>
2020-12-23 19:09:15 +0530

    Updated Metadata
    Signed-off-by: dhruv0000 <patel.4@iitj.ac.in>

--- spec/metadata.pb.go
+++ spec/metadata.pb.go
@@ -25,53 +25,6 @@ const (
 // of the legacy proto package is being used.
 const _ = proto.ProtoPackageIsVersion4
 
-//Controller Health
-type ControllerStatus int32
-
-const (
-	ControllerStatus_RUNNING ControllerStatus = 0
-	ControllerStatus_IDLE    ControllerStatus = 1
-)
-
-// Enum value maps for ControllerStatus.
-var (
-	ControllerStatus_name = map[int32]string{
-		0: "RUNNING",
-		1: "IDLE",
-	}
-	ControllerStatus_value = map[string]int32{
-		"RUNNING": 0,
-		"IDLE":    1,
-	}
-)
-
-func (x ControllerStatus) Enum() *ControllerStatus {
-	p := new(ControllerStatus)
-	*p = x
-	return p
-}
-
-func (x ControllerStatus) String() string {
-	return protoimpl.X.EnumStringOf(x.Descriptor(), protoreflect.EnumNumber(x))
-}
-
-func (ControllerStatus) Descriptor() protoreflect.EnumDescriptor {
-	return file_protos_metadata_proto_enumTypes[0].Descriptor()
-}
-
-func (ControllerStatus) Type() protoreflect.EnumType {
-	return &file_protos_metadata_proto_enumTypes[0]
-}
-
-func (x ControllerStatus) Number() protoreflect.EnumNumber {
-	return protoreflect.EnumNumber(x)
-}
-
-// Deprecated: Use ControllerStatus.Descriptor instead.
-func (ControllerStatus) EnumDescriptor() ([]byte, []int) {
-	return file_protos_metadata_proto_rawDescGZIP(), []int{0}
-}
-
 type ApplicationMetadata_ApplicationProtocol int32
 
 const (
@@ -105,11 +58,11 @@ func (x ApplicationMetadata_ApplicationProtocol) String() string {
 }
 
 func (ApplicationMetadata_ApplicationProtocol) Descriptor() protoreflect.EnumDescriptor {
-	return file_protos_metadata_proto_enumTypes[1].Descriptor()
+	return file_protos_metadata_proto_enumTypes[0].Descriptor()
 }
 
 func (ApplicationMetadata_ApplicationProtocol) Type() protoreflect.EnumType {
-	return &file_protos_metadata_proto_enumTypes[1]
+	return &file_protos_metadata_proto_enumTypes[0]
 }
 
 func (x ApplicationMetadata_ApplicationProtocol) Number() protoreflect.EnumNumber {
@@ -327,197 +280,6 @@ func (x *TrafficMetadata) GetPingHealthEndpoint() string {
 	return ""
 }
 
-type ControllerHealth struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	Status ControllerStatus `protobuf:"varint,1,opt,name=status,proto3,enum=smp.ControllerStatus" json:"status,omitempty"`
-}
-
-func (x *ControllerHealth) Reset() {
-	*x = ControllerHealth{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_protos_metadata_proto_msgTypes[2]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
-}
-
-func (x *ControllerHealth) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*ControllerHealth) ProtoMessage() {}
-
-func (x *ControllerHealth) ProtoReflect() protoreflect.Message {
-	mi := &file_protos_metadata_proto_msgTypes[2]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use ControllerHealth.ProtoReflect.Descriptor instead.
-func (*ControllerHealth) Descriptor() ([]byte, []int) {
-	return file_protos_metadata_proto_rawDescGZIP(), []int{2}
-}
-
-func (x *ControllerHealth) GetStatus() ControllerStatus {
-	if x != nil {
-		return x.Status
-	}
-	return ControllerStatus_RUNNING
-}
-
-//Controller Info
-type ControllerInfo struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	Name    string `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
-	Version string `protobuf:"bytes,2,opt,name=version,proto3" json:"version,omitempty"`
-}
-
-func (x *ControllerInfo) Reset() {
-	*x = ControllerInfo{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_protos_metadata_proto_msgTypes[3]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
-}
-
-func (x *ControllerInfo) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*ControllerInfo) ProtoMessage() {}
-
-func (x *ControllerInfo) ProtoReflect() protoreflect.Message {
-	mi := &file_protos_metadata_proto_msgTypes[3]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use ControllerInfo.ProtoReflect.Descriptor instead.
-func (*ControllerInfo) Descriptor() ([]byte, []int) {
-	return file_protos_metadata_proto_rawDescGZIP(), []int{3}
-}
-
-func (x *ControllerInfo) GetName() string {
-	if x != nil {
-		return x.Name
-	}
-	return ""
-}
-
-func (x *ControllerInfo) GetVersion() string {
-	if x != nil {
-		return x.Version
-	}
-	return ""
-}
-
-//Errors
-type CommonError struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-
-	Code                 string `protobuf:"bytes,1,opt,name=code,proto3" json:"code,omitempty"`
-	Severity             string `protobuf:"bytes,2,opt,name=severity,proto3" json:"severity,omitempty"`
-	ShortDescription     string `protobuf:"bytes,3,opt,name=short_description,json=shortDescription,proto3" json:"short_description,omitempty"`
-	LongDescription      string `protobuf:"bytes,4,opt,name=long_description,json=longDescription,proto3" json:"long_description,omitempty"`
-	ProbableDause        string `protobuf:"bytes,5,opt,name=probable_dause,json=probableDause,proto3" json:"probable_dause,omitempty"`
-	SuggestedRemediation string `protobuf:"bytes,6,opt,name=suggested_remediation,json=suggestedRemediation,proto3" json:"suggested_remediation,omitempty"`
-}
-
-func (x *CommonError) Reset() {
-	*x = CommonError{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_protos_metadata_proto_msgTypes[4]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
-}
-
-func (x *CommonError) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*CommonError) ProtoMessage() {}
-
-func (x *CommonError) ProtoReflect() protoreflect.Message {
-	mi := &file_protos_metadata_proto_msgTypes[4]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use CommonError.ProtoReflect.Descriptor instead.
-func (*CommonError) Descriptor() ([]byte, []int) {
-	return file_protos_metadata_proto_rawDescGZIP(), []int{4}
-}
-
-func (x *CommonError) GetCode() string {
-	if x != nil {
-		return x.Code
-	}
-	return ""
-}
-
-func (x *CommonError) GetSeverity() string {
-	if x != nil {
-		return x.Severity
-	}
-	return ""
-}
-
-func (x *CommonError) GetShortDescription() string {
-	if x != nil {
-		return x.ShortDescription
-	}
-	return ""
-}
-
-func (x *CommonError) GetLongDescription() string {
-	if x != nil {
-		return x.LongDescription
-	}
-	return ""
-}
-
-func (x *CommonError) GetProbableDause() string {
-	if x != nil {
-		return x.ProbableDause
-	}
-	return ""
-}
-
-func (x *CommonError) GetSuggestedRemediation() string {
-	if x != nil {
-		return x.SuggestedRemediation
-	}
-	return ""
-}
-
 var File_protos_metadata_proto protoreflect.FileDescriptor
 
 var file_protos_metadata_proto_rawDesc = []byte{
@@ -575,38 +337,12 @@ var file_protos_metadata_proto_rawDesc = []byte{
 	0x09, 0x75, 0x73, 0x65, 0x72, 0x41, 0x67, 0x65, 0x6e, 0x74, 0x12, 0x30, 0x0a, 0x14, 0x70, 0x69,
 	0x6e, 0x67, 0x5f, 0x68, 0x65, 0x61, 0x6c, 0x74, 0x68, 0x5f, 0x65, 0x6e, 0x64, 0x70, 0x6f, 0x69,
 	0x6e, 0x74, 0x18, 0x07, 0x20, 0x01, 0x28, 0x09, 0x52, 0x12, 0x70, 0x69, 0x6e, 0x67, 0x48, 0x65,
-	0x61, 0x6c, 0x74, 0x68, 0x45, 0x6e, 0x64, 0x70, 0x6f, 0x69, 0x6e, 0x74, 0x22, 0x41, 0x0a, 0x10,
-	0x43, 0x6f, 0x6e, 0x74, 0x72, 0x6f, 0x6c, 0x6c, 0x65, 0x72, 0x48, 0x65, 0x61, 0x6c, 0x74, 0x68,
-	0x12, 0x2d, 0x0a, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0e,
-	0x32, 0x15, 0x2e, 0x73, 0x6d, 0x70, 0x2e, 0x43, 0x6f, 0x6e, 0x74, 0x72, 0x6f, 0x6c, 0x6c, 0x65,
-	0x72, 0x53, 0x74, 0x61, 0x74, 0x75, 0x73, 0x52, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x22,
-	0x3e, 0x0a, 0x0e, 0x43, 0x6f, 0x6e, 0x74, 0x72, 0x6f, 0x6c, 0x6c, 0x65, 0x72, 0x49, 0x6e, 0x66,
-	0x6f, 0x12, 0x12, 0x0a, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52,
-	0x04, 0x6e, 0x61, 0x6d, 0x65, 0x12, 0x18, 0x0a, 0x07, 0x76, 0x65, 0x72, 0x73, 0x69, 0x6f, 0x6e,
-	0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x76, 0x65, 0x72, 0x73, 0x69, 0x6f, 0x6e, 0x22,
-	0xf1, 0x01, 0x0a, 0x0b, 0x43, 0x6f, 0x6d, 0x6d, 0x6f, 0x6e, 0x45, 0x72, 0x72, 0x6f, 0x72, 0x12,
-	0x12, 0x0a, 0x04, 0x63, 0x6f, 0x64, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x63,
-	0x6f, 0x64, 0x65, 0x12, 0x1a, 0x0a, 0x08, 0x73, 0x65, 0x76, 0x65, 0x72, 0x69, 0x74, 0x79, 0x18,
-	0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x08, 0x73, 0x65, 0x76, 0x65, 0x72, 0x69, 0x74, 0x79, 0x12,
-	0x2b, 0x0a, 0x11, 0x73, 0x68, 0x6f, 0x72, 0x74, 0x5f, 0x64, 0x65, 0x73, 0x63, 0x72, 0x69, 0x70,
-	0x74, 0x69, 0x6f, 0x6e, 0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x52, 0x10, 0x73, 0x68, 0x6f, 0x72,
-	0x74, 0x44, 0x65, 0x73, 0x63, 0x72, 0x69, 0x70, 0x74, 0x69, 0x6f, 0x6e, 0x12, 0x29, 0x0a, 0x10,
-	0x6c, 0x6f, 0x6e, 0x67, 0x5f, 0x64, 0x65, 0x73, 0x63, 0x72, 0x69, 0x70, 0x74, 0x69, 0x6f, 0x6e,
-	0x18, 0x04, 0x20, 0x01, 0x28, 0x09, 0x52, 0x0f, 0x6c, 0x6f, 0x6e, 0x67, 0x44, 0x65, 0x73, 0x63,
-	0x72, 0x69, 0x70, 0x74, 0x69, 0x6f, 0x6e, 0x12, 0x25, 0x0a, 0x0e, 0x70, 0x72, 0x6f, 0x62, 0x61,
-	0x62, 0x6c, 0x65, 0x5f, 0x64, 0x61, 0x75, 0x73, 0x65, 0x18, 0x05, 0x20, 0x01, 0x28, 0x09, 0x52,
-	0x0d, 0x70, 0x72, 0x6f, 0x62, 0x61, 0x62, 0x6c, 0x65, 0x44, 0x61, 0x75, 0x73, 0x65, 0x12, 0x33,
-	0x0a, 0x15, 0x73, 0x75, 0x67, 0x67, 0x65, 0x73, 0x74, 0x65, 0x64, 0x5f, 0x72, 0x65, 0x6d, 0x65,
-	0x64, 0x69, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x18, 0x06, 0x20, 0x01, 0x28, 0x09, 0x52, 0x14, 0x73,
-	0x75, 0x67, 0x67, 0x65, 0x73, 0x74, 0x65, 0x64, 0x52, 0x65, 0x6d, 0x65, 0x64, 0x69, 0x61, 0x74,
-	0x69, 0x6f, 0x6e, 0x2a, 0x29, 0x0a, 0x10, 0x43, 0x6f, 0x6e, 0x74, 0x72, 0x6f, 0x6c, 0x6c, 0x65,
-	0x72, 0x53, 0x74, 0x61, 0x74, 0x75, 0x73, 0x12, 0x0b, 0x0a, 0x07, 0x52, 0x55, 0x4e, 0x4e, 0x49,
-	0x4e, 0x47, 0x10, 0x00, 0x12, 0x08, 0x0a, 0x04, 0x49, 0x44, 0x4c, 0x45, 0x10, 0x01, 0x42, 0x3c,
-	0x0a, 0x07, 0x63, 0x6f, 0x6d, 0x2e, 0x73, 0x6d, 0x70, 0x42, 0x0d, 0x4d, 0x65, 0x74, 0x61, 0x64,
-	0x61, 0x74, 0x61, 0x50, 0x72, 0x6f, 0x74, 0x6f, 0x50, 0x01, 0x5a, 0x08, 0x73, 0x70, 0x65, 0x63,
-	0x3b, 0x73, 0x6d, 0x70, 0xa2, 0x02, 0x03, 0x73, 0x6d, 0x70, 0xaa, 0x02, 0x03, 0x53, 0x6d, 0x70,
-	0xca, 0x02, 0x03, 0x53, 0x6d, 0x70, 0xea, 0x02, 0x03, 0x53, 0x6d, 0x70, 0x62, 0x06, 0x70, 0x72,
-	0x6f, 0x74, 0x6f, 0x33,
+	0x61, 0x6c, 0x74, 0x68, 0x45, 0x6e, 0x64, 0x70, 0x6f, 0x69, 0x6e, 0x74, 0x42, 0x3c, 0x0a, 0x07,
+	0x63, 0x6f, 0x6d, 0x2e, 0x73, 0x6d, 0x70, 0x42, 0x0d, 0x4d, 0x65, 0x74, 0x61, 0x64, 0x61, 0x74,
+	0x61, 0x50, 0x72, 0x6f, 0x74, 0x6f, 0x50, 0x01, 0x5a, 0x08, 0x73, 0x70, 0x65, 0x63, 0x3b, 0x73,
+	0x6d, 0x70, 0xa2, 0x02, 0x03, 0x73, 0x6d, 0x70, 0xaa, 0x02, 0x03, 0x53, 0x6d, 0x70, 0xca, 0x02,
+	0x03, 0x53, 0x6d, 0x70, 0xea, 0x02, 0x03, 0x53, 0x6d, 0x70, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74,
+	0x6f, 0x33,
 }
 
 var (
@@ -621,25 +357,20 @@ func file_protos_metadata_proto_rawDescGZIP() []byte {
 	return file_protos_metadata_proto_rawDescData
 }
 
-var file_protos_metadata_proto_enumTypes = make([]protoimpl.EnumInfo, 2)
-var file_protos_metadata_proto_msgTypes = make([]protoimpl.MessageInfo, 5)
+var file_protos_metadata_proto_enumTypes = make([]protoimpl.EnumInfo, 1)
+var file_protos_metadata_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
 var file_protos_metadata_proto_goTypes = []interface{}{
-	(ControllerStatus)(0),                        // 0: smp.ControllerStatus
-	(ApplicationMetadata_ApplicationProtocol)(0), // 1: smp.ApplicationMetadata.ApplicationProtocol
-	(*ApplicationMetadata)(nil),                  // 2: smp.ApplicationMetadata
-	(*TrafficMetadata)(nil),                      // 3: smp.TrafficMetadata
-	(*ControllerHealth)(nil),                     // 4: smp.ControllerHealth
-	(*ControllerInfo)(nil),                       // 5: smp.ControllerInfo
-	(*CommonError)(nil),                          // 6: smp.CommonError
+	(ApplicationMetadata_ApplicationProtocol)(0), // 0: smp.ApplicationMetadata.ApplicationProtocol
+	(*ApplicationMetadata)(nil),                  // 1: smp.ApplicationMetadata
+	(*TrafficMetadata)(nil),                      // 2: smp.TrafficMetadata
 }
 var file_protos_metadata_proto_depIdxs = []int32{
-	1, // 0: smp.ApplicationMetadata.application_protocol:type_name -> smp.ApplicationMetadata.ApplicationProtocol
-	0, // 1: smp.ControllerHealth.status:type_name -> smp.ControllerStatus
-	2, // [2:2] is the sub-list for method output_type
-	2, // [2:2] is the sub-list for method input_type
-	2, // [2:2] is the sub-list for extension type_name
-	2, // [2:2] is the sub-list for extension extendee
-	0, // [0:2] is the sub-list for field type_name
+	0, // 0: smp.ApplicationMetadata.application_protocol:type_name -> smp.ApplicationMetadata.ApplicationProtocol
+	1, // [1:1] is the sub-list for method output_type
+	1, // [1:1] is the sub-list for method input_type
+	1, // [1:1] is the sub-list for extension type_name
+	1, // [1:1] is the sub-list for extension extendee
+	0, // [0:1] is the sub-list for field type_name
 }
 
 func init() { file_protos_metadata_proto_init() }
@@ -672,50 +403,14 @@ func file_protos_metadata_proto_init() {
 				return nil
 			}
 		}
-		file_protos_metadata_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*ControllerHealth); i {
-			case 0:
-				return &v.state
-			case 1:
-				return &v.sizeCache
-			case 2:
-				return &v.unknownFields
-			default:
-				return nil
-			}
-		}
-		file_protos_metadata_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*ControllerInfo); i {
-			case 0:
-				return &v.state
-			case 1:
-				return &v.sizeCache
-			case 2:
-				return &v.unknownFields
-			default:
-				return nil
-			}
-		}
-		file_protos_metadata_proto_msgTypes[4].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*CommonError); i {
-			case 0:
-				return &v.state
-			case 1:
-				return &v.sizeCache
-			case 2:
-				return &v.unknownFields
-			default:
-				return nil
-			}
-		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_protos_metadata_proto_rawDesc,
-			NumEnums:      2,
-			NumMessages:   5,
+			NumEnums:      1,
+			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
