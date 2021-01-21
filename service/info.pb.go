28a5dc175510906ea4e068af23056f4f10e6b837
dhruv0000 <patel.4@iitj.ac.in>
2021-01-21 20:22:48 +0530

    Moved Controller and Health to Service package
    Signed-off-by: dhruv0000 <patel.4@iitj.ac.in>

--- service/info.pb.go
+++ service/info.pb.go
@@ -2,9 +2,9 @@
 // versions:
 // 	protoc-gen-go v1.24.0
 // 	protoc        v3.6.1
-// source: controller/info.proto
+// source: service/info.proto
 
-package controller
+package service
 
 import (
 	proto "github.com/golang/protobuf/proto"
@@ -37,7 +37,7 @@ type ControllerInfo struct {
 func (x *ControllerInfo) Reset() {
 	*x = ControllerInfo{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_controller_info_proto_msgTypes[0]
+		mi := &file_service_info_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -50,7 +50,7 @@ func (x *ControllerInfo) String() string {
 func (*ControllerInfo) ProtoMessage() {}
 
 func (x *ControllerInfo) ProtoReflect() protoreflect.Message {
-	mi := &file_controller_info_proto_msgTypes[0]
+	mi := &file_service_info_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -63,7 +63,7 @@ func (x *ControllerInfo) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use ControllerInfo.ProtoReflect.Descriptor instead.
 func (*ControllerInfo) Descriptor() ([]byte, []int) {
-	return file_controller_info_proto_rawDescGZIP(), []int{0}
+	return file_service_info_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *ControllerInfo) GetName() string {
@@ -80,40 +80,39 @@ func (x *ControllerInfo) GetVersion() string {
 	return ""
 }
 
-var File_controller_info_proto protoreflect.FileDescriptor
+var File_service_info_proto protoreflect.FileDescriptor
 
-var file_controller_info_proto_rawDesc = []byte{
-	0x0a, 0x15, 0x63, 0x6f, 0x6e, 0x74, 0x72, 0x6f, 0x6c, 0x6c, 0x65, 0x72, 0x2f, 0x69, 0x6e, 0x66,
-	0x6f, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x0a, 0x63, 0x6f, 0x6e, 0x74, 0x72, 0x6f, 0x6c,
-	0x6c, 0x65, 0x72, 0x22, 0x3e, 0x0a, 0x0e, 0x43, 0x6f, 0x6e, 0x74, 0x72, 0x6f, 0x6c, 0x6c, 0x65,
-	0x72, 0x49, 0x6e, 0x66, 0x6f, 0x12, 0x12, 0x0a, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x18, 0x01, 0x20,
-	0x01, 0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x12, 0x18, 0x0a, 0x07, 0x76, 0x65, 0x72,
-	0x73, 0x69, 0x6f, 0x6e, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x76, 0x65, 0x72, 0x73,
-	0x69, 0x6f, 0x6e, 0x42, 0x44, 0x5a, 0x42, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f,
-	0x6d, 0x2f, 0x6c, 0x61, 0x79, 0x65, 0x72, 0x35, 0x69, 0x6f, 0x2f, 0x73, 0x65, 0x72, 0x76, 0x69,
-	0x63, 0x65, 0x2d, 0x6d, 0x65, 0x73, 0x68, 0x2d, 0x70, 0x65, 0x72, 0x66, 0x6f, 0x72, 0x6d, 0x61,
-	0x6e, 0x63, 0x65, 0x2f, 0x63, 0x6f, 0x6e, 0x74, 0x72, 0x6f, 0x6c, 0x6c, 0x65, 0x72, 0x3b, 0x63,
-	0x6f, 0x6e, 0x74, 0x72, 0x6f, 0x6c, 0x6c, 0x65, 0x72, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f,
-	0x33,
+var file_service_info_proto_rawDesc = []byte{
+	0x0a, 0x12, 0x73, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x2f, 0x69, 0x6e, 0x66, 0x6f, 0x2e, 0x70,
+	0x72, 0x6f, 0x74, 0x6f, 0x12, 0x07, 0x73, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x22, 0x3e, 0x0a,
+	0x0e, 0x43, 0x6f, 0x6e, 0x74, 0x72, 0x6f, 0x6c, 0x6c, 0x65, 0x72, 0x49, 0x6e, 0x66, 0x6f, 0x12,
+	0x12, 0x0a, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e,
+	0x61, 0x6d, 0x65, 0x12, 0x18, 0x0a, 0x07, 0x76, 0x65, 0x72, 0x73, 0x69, 0x6f, 0x6e, 0x18, 0x02,
+	0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x76, 0x65, 0x72, 0x73, 0x69, 0x6f, 0x6e, 0x42, 0x3e, 0x5a,
+	0x3c, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x6c, 0x61, 0x79, 0x65,
+	0x72, 0x35, 0x69, 0x6f, 0x2f, 0x73, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x2d, 0x6d, 0x65, 0x73,
+	0x68, 0x2d, 0x70, 0x65, 0x72, 0x66, 0x6f, 0x72, 0x6d, 0x61, 0x6e, 0x63, 0x65, 0x2f, 0x73, 0x65,
+	0x72, 0x76, 0x69, 0x63, 0x65, 0x3b, 0x73, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x62, 0x06, 0x70,
+	0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
-	file_controller_info_proto_rawDescOnce sync.Once
-	file_controller_info_proto_rawDescData = file_controller_info_proto_rawDesc
+	file_service_info_proto_rawDescOnce sync.Once
+	file_service_info_proto_rawDescData = file_service_info_proto_rawDesc
 )
 
-func file_controller_info_proto_rawDescGZIP() []byte {
-	file_controller_info_proto_rawDescOnce.Do(func() {
-		file_controller_info_proto_rawDescData = protoimpl.X.CompressGZIP(file_controller_info_proto_rawDescData)
+func file_service_info_proto_rawDescGZIP() []byte {
+	file_service_info_proto_rawDescOnce.Do(func() {
+		file_service_info_proto_rawDescData = protoimpl.X.CompressGZIP(file_service_info_proto_rawDescData)
 	})
-	return file_controller_info_proto_rawDescData
+	return file_service_info_proto_rawDescData
 }
 
-var file_controller_info_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
-var file_controller_info_proto_goTypes = []interface{}{
-	(*ControllerInfo)(nil), // 0: controller.ControllerInfo
+var file_service_info_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_service_info_proto_goTypes = []interface{}{
+	(*ControllerInfo)(nil), // 0: service.ControllerInfo
 }
-var file_controller_info_proto_depIdxs = []int32{
+var file_service_info_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
 	0, // [0:0] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -121,13 +120,13 @@ var file_controller_info_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_controller_info_proto_init() }
-func file_controller_info_proto_init() {
-	if File_controller_info_proto != nil {
+func init() { file_service_info_proto_init() }
+func file_service_info_proto_init() {
+	if File_service_info_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_controller_info_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_service_info_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*ControllerInfo); i {
 			case 0:
 				return &v.state
@@ -144,18 +143,18 @@ func file_controller_info_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_controller_info_proto_rawDesc,
+			RawDescriptor: file_service_info_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   1,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
-		GoTypes:           file_controller_info_proto_goTypes,
-		DependencyIndexes: file_controller_info_proto_depIdxs,
-		MessageInfos:      file_controller_info_proto_msgTypes,
+		GoTypes:           file_service_info_proto_goTypes,
+		DependencyIndexes: file_service_info_proto_depIdxs,
+		MessageInfos:      file_service_info_proto_msgTypes,
 	}.Build()
-	File_controller_info_proto = out.File
-	file_controller_info_proto_rawDesc = nil
-	file_controller_info_proto_goTypes = nil
-	file_controller_info_proto_depIdxs = nil
+	File_service_info_proto = out.File
+	file_service_info_proto_rawDesc = nil
+	file_service_info_proto_goTypes = nil
+	file_service_info_proto_depIdxs = nil
 }
