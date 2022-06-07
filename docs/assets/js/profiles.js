c356e49d4678a363581adb841189628d386755fd
asubedy <frexpe@pm.me>
2022-06-08 01:09:38 +0545

    updated profile pagination, added table pagination
    Signed-off-by: asubedy <frexpe@pm.me>

--- docs/assets/js/profiles.js
+++ docs/assets/js/profiles.js
@@ -3,19 +3,85 @@ pages = document.getElementById("pages")
 
 let URL = window.location.href
 console.log(URL)
-let pageNumber = parseInt(URL.slice(-1))
+let currentPage = parseInt(URL.slice(-1))
 
-if(pageNumber==="t"){
-  pageNumber = 1;
+if(currentPage==="t"){
+  currentPage = 1;
 }
-function pageClick(number){
-  console.log(number)
-  indPage = document.getElementById(`pageNumber${number}`)
-
-  indPage.innerHTML += `<span class="sr-only">(current)</span>`
+function reloadFunction(page){
+  window.location.replace(`${location.origin}/dashboard/burst/page=${page}`)
 }
 
-fetch(`https://meshery.layer5.io/smp/performance/profiles/?page=${pageNumber-1}`, { 
+function createPagination(pages, page) {
+  let str = '<ul class="pagination justify-content-center">';
+  let active;
+  let pageCutLow = page - 1;
+  let pageCutHigh = page + 1;
+  // Show the Previous button only if you are on a page other than the first
+  if (page > 1) {
+    str += `<li class="page-item previous no"><a onclick="reloadFunction(${page-1})" class = "page-link" onclick="createPagination(${pages}, ${page-1})">Previous</a></li>`;
+  }
+  // Show all the pagination elements if there are less than 6 pages total
+  if (pages < 6) {
+    for (let p = 1; p <= pages; p++) {
+      active = page == p ? "active" : "no";
+      str += `<li class=" page-item ${active}"><a onclick="reloadFunction(${p})" class = "page-link" onclick="createPagination(${pages}, ${p})"> ${p} </a></li>`;
+    }
+  }
+  // Use "..." to collapse pages outside of a certain range
+  else {
+    // Show the very first page followed by a "..." at the beginning of the
+    // pagination section (after the Previous button)
+    if (page > 2) {
+      str += `<li class="no page-item"><a onclick="reloadFunction(${1})" class = "page-link" onclick="createPagination(${pages}, ${1})">1</a></li>`;
+      if (page > 3) {
+          str += `<li class="out-of-range"><a onclick="reloadFunction(${page-2})" class = "page-link" onclick="createPagination(${pages},${page-2})">...</a></li>`;
+      }
+    }
+    // Determine how many pages to show after the current page index
+    if (page === 1) {
+      pageCutHigh += 2;
+    } else if (page === 2) {
+      pageCutHigh += 1;
+    }
+    // Determine how many pages to show before the current page index
+    if (page === pages) {
+      pageCutLow -= 2;
+    } else if (page === pages-1) {
+      pageCutLow -= 1;
+    }
+    // Output the indexes for pages that fall inside the range of pageCutLow
+    // and pageCutHigh
+    for (let p = pageCutLow; p <= pageCutHigh; p++) {
+      if (p === 0) {
+        p += 1;
+      }
+      if (p > pages) {
+        continue
+      }
+      active = page == p ? "active" : "no";
+      str += `<li class="page-item ${active}"><a onclick="reloadFunction(${p})" class = "page-link" onclick="createPagination(${pages}, ${p})">${p}</a></li>`;
+    }
+    // Show the very last page preceded by a "..." at the end of the pagination
+    // section (before the Next button)
+    if (page < pages-1) {
+      if (page < pages-2) {
+        str += `<li class="out-of-range"><a onclick="reloadFunction(${page+2})" class = "page-link" onclick="createPagination(${pages},${page+2})">...</a></li>`;
+      }
+      str += `<li class="page-item no"><a onclick="reloadFunction(${pages})" class = "page-link" onclick="createPagination(${pages}, ${pages})">${pages}</a></li>`;
+    }
+  }
+  // Show the Next button only if you are on a page other than the last
+  if (page < pages) {
+    str += `<li class="page-item next no"><a onclick="reloadFunction(${page+1})" class = "page-link" onclick="createPagination(${pages}, ${page+1})">Next</a></li>`;
+  }
+  str += '</ul>';
+  // Return the pagination string to be outputted in the pug templates
+  document.getElementById('paginations').innerHTML = str;
+  return str;
+}
+
+fetch(`https://meshery.layer5.io/smp/performance/profiles/?page=${currentPage-1}`, { 
     method: "GET"
   }).then(function(response) {
     return response.json();
@@ -31,7 +97,7 @@ fetch(`https://meshery.layer5.io/smp/performance/profiles/?page=${pageNumber-1}`
                         <p class="card-text">
                             Performance profile number ${i+1}
                         </p>
-                         <a href="${location.origin}/dashboard/performance#${data.profiles[i].id}"  class="btn btn-primary">Show Results</a>
+                         <a href="${location.origin}/dashboard/performance#${data.profiles[i].id}/page=1"  class="btn btn-primary">Show Results</a>
                     </div>
                  </div>
             </div>
@@ -41,23 +107,8 @@ fetch(`https://meshery.layer5.io/smp/performance/profiles/?page=${pageNumber-1}`
     let numberOfPages = Math.ceil(data.total_count/10);
     console.log(numberOfPages)
 
-    pages.innerHTML = `<li class="page-item">
-    <a class="page-link" href="#" aria-label="Previous">
-      <span aria-hidden="true">&laquo;</span>
-      <span class="sr-only">Previous</span>
-    </a>
-  </li>`
+    document.getElementById('paginations').innerHTML = createPagination(numberOfPages, currentPage);
 
-  for(let j = 0;j<numberOfPages;j++){
-    pages.innerHTML +=  `<li onclick ="pageClick(${j+1})" class="page-item"><a id=pageNumber${j+1} class="page-link" href="${location.origin}/dashboard/burst/page=${j+1}">${j+1}</a></li>`
-  }
-
-  pages.innerHTML += `<li class="page-item">
-  <a class="page-link" href="#" aria-label="Next">
-    <span aria-hidden="true">&raquo;</span>
-    <span class="sr-only">Next</span>
-  </a>
-</li>`
     console.log(content);
     cardRow.innerHTML = content;
   })
--- docs/assets/js/table.js
+++ docs/assets/js/table.js
@@ -1,9 +1,92 @@
 
 tableBody = document.getElementById("table-body")
-let text = window.location.hash.substring(1)
+let URL = window.location.hash.substring(1)
+let profileId = URL.slice(0,36)
+
+let currentPage = parseInt(URL.slice(42))
+
+console.log(profileId, currentPage)
+
+function reloadFunction(profileIds,page){
+  window.location.replace(`${location.origin}/dashboard/performance/#${profileIds}/page=${page}`)
+  window.location.reload()
+}
 
 
-fetch(`https://meshery.layer5.io/smp/performance/profiles/${text}/results`, { 
+function createPagination(pages, page,profileIds) {
+  console.log(typeof(profileIds))
+  let str = '<ul class="pagination justify-content-center">';
+  let active;
+  let pageCutLow = page - 1;
+  let pageCutHigh = page + 1;
+  // Show the Previous button only if you are on a page other than the first
+  if (page > 1) {
+    str += `<li class="page-item previous no"><a onclick="reloadFunction('${profileIds}',${page-1})" href="${location.origin}/dashboard/performance/#${profileIds}/page=${page-1}" class = "page-link" onclick="createPagination(${pages}, ${page-1}, '${profileIds}')">Previous</a></li>`;
+  }
+  // Show all the pagination elements if there are less than 6 pages total
+  if (pages < 6) {
+    for (let p = 1; p <= pages; p++) {
+      active = page == p ? "active" : "no";
+      str += `<li class="${active}"><a onclick="reloadFunction('${profileIds}',${p})" href="${location.origin}/dashboard/performance/#${profileIds}/${p}" class = "page-link" onclick="createPagination(${pages}, ${p}, '${profileIds}')"> ${p} </a></li>`;
+    }
+  }
+  // Use "..." to collapse pages outside of a certain range
+  else {
+    // Show the very first page followed by a "..." at the beginning of the
+    // pagination section (after the Previous button)
+    if (page > 2) {
+      str += `<li class="no page-item"><a onclick="reloadFunction('${profileIds}',${1})" class = "page-link" onclick="createPagination(${pages}, ${1}, '${profileIds}')">1</a></li>`;
+      if (page > 3) {
+          str += `<li class="out-of-range"><a onclick="reloadFunction('${profileIds}',${page-2})" class = "page-link" onclick="createPagination(${pages},${page-2}, '${profileIds}')">...</a></li>`;
+      }
+    }
+    // Determine how many pages to show after the current page index
+    if (page === 1) {
+      pageCutHigh += 2;
+    } else if (page === 2) {
+      pageCutHigh += 1;
+    }
+    // Determine how many pages to show before the current page index
+    if (page === pages) {
+      pageCutLow -= 2;
+    } else if (page === pages-1) {
+      pageCutLow -= 1;
+    }
+    // Output the indexes for pages that fall inside the range of pageCutLow
+    // and pageCutHigh
+    for (let p = pageCutLow; p <= pageCutHigh; p++) {
+      if (p === 0) {
+        p += 1;
+      }
+      if (p > pages) {
+        continue
+      }
+      active = page == p ? "active" : "no";
+      str += `<li class="page-item ${active}"><a onclick="reloadFunction('${profileIds}',${p})" class = "page-link" onclick="createPagination(${pages}, ${p}, '${profileIds}')">${p}</a></li>`;
+    }
+    // Show the very last page preceded by a "..." at the end of the pagination
+    // section (before the Next button)
+    if (page < pages-1) {
+      if (page < pages-2) {
+        str += `<li class="out-of-range"><a onclick="reloadFunction('${profileIds}',${page+2})" class = "page-link" onclick="createPagination(${pages},${page+2}, '${profileIds}')">...</a></li>`;
+      }
+      str += `<li class="page-item no"><a onclick="reloadFunction('${profileIds}',${pages})" class = "page-link" onclick="createPagination(${pages}, ${pages},'${profileIds}')">${pages}</a></li>`;
+    }
+  }
+  // Show the Next button only if you are on a page other than the last
+  if (page < pages) {
+    str += `<li class="page-item next no"><a onclick="reloadFunction('${profileIds}',${page+1})" class = "page-link" onclick="createPagination(${pages}, ${page+1}, '${profileIds}')">Next</a></li>`;
+  }
+  str += '</ul>';
+  // Return the pagination string to be outputted in the pug templates
+  document.getElementById('paginations').innerHTML = str;
+  return str;
+}
+
+
+
+
+fetch(`https://meshery.layer5.io/smp/performance/profiles/${profileId}/results/?page=${currentPage-1}`, { 
     method: "GET"
   }).then(function(response) {
     return response.json();
@@ -13,13 +96,19 @@ fetch(`https://meshery.layer5.io/smp/performance/profiles/${text}/results`, {
         content += `
             <tr>
                 <th scope="row">${i+1}</th>
-                <td>  <a href="${location.origin}/dashboard/performance/individual#${text}#${i}">${data.results[i].name}</a></td>
+                <td>  <a href="${location.origin}/dashboard/performance/individual#${profileId}#${i}">${data.results[i].name}</a></td>
                 <td>${data.results[i].runner_results.URL}</td>
                 <td>${data.results[i].updated_at}</td>
                 <td>${data.results[i].mesh}</td>
             </tr>
         `
     }
+
+    let numberOfPages = Math.ceil(data.total_count/10);
+    console.log(numberOfPages)
+
+    document.getElementById('paginations').innerHTML = createPagination(numberOfPages, currentPage, profileId);
+
     console.log(content);
     tableBody.innerHTML = content;
   })
